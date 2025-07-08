66079d8b563b16b4cfdb5205e0d9c92e3ef70c3f
stefv21 <161451930+stefv21@users.noreply.github.com>
2025-07-08 07:51:44 -0400

    created metro file

--- App.js
+++ App.js
@@ -10,18 +10,6 @@ import Start from './components/Start';
 import Chat from './components/Chat';
 import { db } from './firebaseConfig';
 
-// Your Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyBUJLBenfALRtIBcYAOfIHmbdIGOcQYo78",
-  authDomain: "chat-4b65c.firebaseapp.com",
-  projectId: "chat-4b65c",
-  storageBucket: "chat-4b65c.appspot.com",    // note: use .appspot.com
-  messagingSenderId: "752625705370",
-  appId: "1:752625705370:web:d52be3d35ab01683529a23"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 const Stack = createStackNavigator();
 
